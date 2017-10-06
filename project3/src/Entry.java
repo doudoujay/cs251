dbabcb97852651eb153cbab9d234365deda3764b
doudoujay <doudoujay@qq.com>
2017-10-06 14:38:17 -0400

    .

--- project3/src/Entry.java
+++ project3/src/Entry.java
@@ -4,6 +4,7 @@ import java.util.Comparator;
 public class Entry implements Comparable<Entry> {
   private int x;
   private int y;
+  private int y_rank;
 
 
   public Entry(int x, int y) {
@@ -27,9 +28,35 @@ public class Entry implements Comparable<Entry> {
     this.y = y;
   }
 
+  public int getY_rank() {
+    return y_rank;
+  }
+
+  public void setY_rank(int y_rank) {
+    this.y_rank = y_rank;
+  }
+
   @Override
   public int compareTo(Entry o) {
     return Comparators.X.compare(this, o);
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    Entry entry = (Entry) o;
+
+    if (x != entry.x) return false;
+    return y == entry.y;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = x;
+    result = 31 * result + y;
+    return result;
   }
 
   @Override
@@ -49,6 +76,12 @@ public class Entry implements Comparable<Entry> {
       @Override
       public int compare(Entry o1, Entry o2) {
         return Integer.compare(o1.getY(), o2.getY());
+      }
+    };
+    public static Comparator<Entry> Y_reversed = new Comparator<Entry>() {
+      @Override
+      public int compare(Entry o1, Entry o2) {
+        return Integer.compare(o2.getY(),o1.getY());
       }
     };
   }
--- project3/src/Filter.java
+++ project3/src/Filter.java
@@ -1,98 +1,106 @@
-import edu.princeton.cs.algs4.*;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.Stopwatch;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Iterator;
+import java.util.*;
 
 public class Filter {
-  ArrayList<Entry> entries;
-  int size;
-  public Filter(int size) {
-    this.size = size;
-    this.entries = new ArrayList<Entry>(size);
+    ArrayList<Entry> entries;
+    int size;
+    int maxIndex;
 
-  }
+    public Filter(int size) {
+        this.size = size;
+        this.entries = new ArrayList<Entry>(size);
 
-  public ArrayList<Entry> getEntries() {
-    return entries;
-  }
+    }
 
-  public void setEntries(ArrayList<Entry> entries) {
-    this.entries = entries;
-  }
+    public ArrayList<Entry> getEntries() {
+        return entries;
+    }
 
-  public static void main(String[] args) {
-    Stopwatch s = new Stopwatch();
+    public void setEntries(ArrayList<Entry> entries) {
+        this.entries = entries;
+    }
+
+    public static void main(String[] args) {
+        Stopwatch s = new Stopwatch();
 //    Stdin Workaround
-    if(args.length > 1)
-      try {
-        System.setIn(new FileInputStream(args[1]));
-      } catch (FileNotFoundException e) {
-        e.printStackTrace();
-      }
+//        if (args.length > 1)
+//            try {
+//                System.setIn(new FileInputStream(args[1]));
+//            } catch (FileNotFoundException e) {
+//                e.printStackTrace();
+//            }
 
-    int size = StdIn.readInt(); //set size
-    Filter filter = new Filter(size);
-    for (int i = 0; i < size; i++) {
-      int x = StdIn.readInt();
-      int y = StdIn.readInt();
-      Entry entry = new Entry(x,y);
-      filter.getEntries().add(entry);
-    }
-    filter.filt();
+        int size = StdIn.readInt(); //set size
+        Filter filter = new Filter(size);
+        for (int i = 0; i < size; i++) {
+            int x = StdIn.readInt();
+            int y = StdIn.readInt();
+            Entry entry = new Entry(x, y);
+            filter.getEntries().add(entry);
+        }
+        filter.filt();
 //    Print result
-    for (Entry e:filter.getEntries()
-         ) {
-      System.out.println(e);
-    }
-    System.out.println(s.elapsedTime());
+        for (Entry e : filter.getEntries()
+                ) {
+            System.out.println(e);
+        }
+        System.out.println(s.elapsedTime());
 
-
-
-
-
-  }
-
-  public void filt(){
-    ArrayList<Entry> entries_y_sorted= new ArrayList<Entry>(this.entries);
-    Collections.sort(this.entries); //sort along x for entries
-    Collections.sort(entries_y_sorted,Entry.Comparators.Y); //Sort along y
-
-
-    int index= 0;
-
-    for(Iterator<Entry> iter = this.entries.iterator();iter.hasNext();){
-      Entry entry = iter.next();
-      if(!isValid(entry,index)){
-        iter.remove();
-      }else{
-        index++;
-      }
 
     }
+
+    public void filt() {
+        Collections.sort(this.entries, Entry.Comparators.Y_reversed); //Sort along y
+        LinkedHashMap<Integer, Entry> m = new LinkedHashMap<Integer, Entry>();
+        for (int i = 0; i < this.size; i++) {
+            entries.get(i).setY_rank(i);
+            m.put(i, entries.get(i));
+        }
+        Collections.sort(this.entries); //sort along x for entries
+
+        int index = 0;
+        this.maxIndex = this.size - 1;
+
+        for (Iterator<Entry> iter = this.entries.iterator(); iter.hasNext(); ) {
+            Entry entry = iter.next();
+            if (!isValid(entry, m)) {
+                iter.remove();
+            }
+            System.out.println(index + "/" + size);
+
+            index++;
+        }
 //    Get rid off the inferior
-  }
+    }
 
-  public boolean isValid(Entry entry, int index) {
+    public boolean isValid(Entry entry, LinkedHashMap<Integer, Entry> m) {
 //        from S every record (x[i],y[i]) for which there
 // exists another record (x[j],y[j]) having both x[j] > x[i] and y[j] > y[i].
 //    applied to sorted entries
-    for (int i = index+1; i < this.entries.size(); i++) {
-      if(entry.getY() < this.entries.get(i).getY()) return false;
+//    for (int i = index+1; i < this.entries.size(); i++) {
+//      if(entry.getY() < this.entries.get(i).getY()) return false;
+//    }
+        Map.Entry<Integer,Entry> e = m.entrySet().iterator().next(); // first item
+
+        if (entry.getY() == e.getValue().getY()) {
+//            Compare entry y with the max y
+            m.remove(entry.getY_rank());
+
+            return true;
+        } else {
+//            Smaller than the max sorted list
+            m.remove(entry.getY_rank());
+
+
+            return false;
+        }
+
+
     }
-
-    return true;
-
-  }
-
-
-
-
 
 
 }
